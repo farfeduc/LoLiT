0ea61b9c63da93fac64555b25261512694e20f3b
Adrien Girardeau <adrigir25@gmail.com>
2016-03-06 20:35:47 +0100

    New data chart with championpicks

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,87 +19,85 @@ LoLiT.controller('LoLiTCtrl', ['$scope', '$rootScope', '$http', function($scope,
   $scope.$watch(function() {
   	return $scope.sectionSelectionnee;
   }, function(newSection){
-  	console.log(newSection);
+  	//console.log(newSection);
   });
 
-  /* Chart options test */
-  $scope.options = {
+  // API Management
+  $http.get('/api/championpick/find').success(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      data[i].index = i;
+    }
+    $scope.items = data;
+  });
+
+  $http.get('/api/championban/find').success(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      data[i].index = i;
+    }
+    $scope.items = data;
+  });
+
+  $http.get('/api/totalchampions/find').success(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      data[i].index = i;
+    }
+    $scope.items = data;
+  });
+
+  $http.get('/api/totalchampionban/find').success(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      data[i].index = i;
+    }
+    $scope.items = data;
+  });
+
+  $http.get('/api/winpercentage/find').success(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      data[i].index = i;
+    }
+    $scope.items = data;
+  });
+
+
+  /* Charts data arrays*/
+  // $scope.datapicks = [{
+  //   key: "Champions Pick",
+  //   values: []
+  //   }];
+  $scope.datapicks = [];
+
+
+  //Getting data to display
+  $http({method : 'GET',url : 'http://localhost:1337/api/championpick/find'})
+    .success(function(data, status) {
+        $scope.datapicks = data;
+        //$scope.datapicks.values = data;
+        //console.log($scope.datapicks);
+    })
+    .error(function(data, status) {
+        console.log("Error getting api data array (championpick)");
+    });
+
+  /* Chart options */
+  $scope.optionspicks = {
       chart: {
-          type: 'discreteBarChart',
-          height: 450,
-          margin : {
-              top: 20,
-              right: 20,
-              bottom: 60,
-              left: 55
-          },
-          x: function(d){ return d.label; },
-          y: function(d){ return d.value; },
-          showValues: true,
-          valueFormat: function(d){
-              return d3.format(',.4f')(d);
-          },
-          transitionDuration: 500,
-          xAxis: {
-              axisLabel: 'X Axis'
-          },
-          yAxis: {
-              axisLabel: 'Y Axis',
-              axisLabelDistance: 30
+          type: 'pieChart',
+          height: 500,
+          x: function(d){return d.id;},
+          y: function(d){return d.total;},
+          showLabels: true,
+          duration: 500,
+          labelThreshold: 0.01,
+          labelSunbeamLayout: true,
+          legend: {
+              margin: {
+                  top: 5,
+                  right: 35,
+                  bottom: 5,
+                  left: 0
+              }
           }
       }
   };
-
-  /* Chart data test */
-  $scope.data = [{
-    key: "Cumulative Return",
-    values: [
-        { "label" : "A" , "value" : -29.765957771107 },
-        { "label" : "B" , "value" : 0 },
-        { "label" : "C" , "value" : 32.807804682612 },
-        { "label" : "D" , "value" : 196.45946739256 },
-        { "label" : "E" , "value" : 0.19434030906893 },
-        { "label" : "F" , "value" : -98.079782601442 },
-        { "label" : "G" , "value" : -13.925743130903 },
-        { "label" : "H" , "value" : -5.1387322875705 }
-        ]
-    }];
-
-
-    // API Management
-    $http.get('/championpick/find').success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        data[i].index = i;
-      }
-      $scope.items = data;
-    });
-
-    $http.get('/championban/find').success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        data[i].index = i;
-      }
-      $scope.items = data;
-    });
-
-    $http.get('/totalchampions/find').success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        data[i].index = i;
-      }
-      $scope.items = data;
-    });
-
-    $http.get('/totalchampionban/find').success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        data[i].index = i;
-      }
-      $scope.items = data;
-    });
-
-    $http.get('/winpercentage/find').success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        data[i].index = i;
-      }
-      $scope.items = data;
-    });
 
 }]);
