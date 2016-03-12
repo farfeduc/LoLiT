3934ba69b99947eaa3538f5ea88a5f31d4f2445e
Iwerzhon <scapegoat.minijeux@gmail.com>
2016-03-12 13:12:06 +0100

    Adding Bar Chart in part3

--- assets/js/LoLiTCtrlPart3.js
+++ assets/js/LoLiTCtrlPart3.js
@@ -11,4 +11,67 @@ app.controller('LoLiTCtrlPart3', ['$scope', '$rootScope', '$http', function($sco
   }, function(newSection){
     //console.log(newSection);
   });
+
+  $scope.barchart = {
+            chart: {
+                type: 'discreteBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 50,
+                    left: 55
+                },
+                x: function(d){return d.label;},
+                y: function(d){return d.value + (1e-10);},
+                showValues: true,
+                valueFormat: function(d){
+                    return d3.format(',.0f')(d);
+                },
+                duration: 500,
+                xAxis: {
+                    axisLabel: 'Jour'
+                },
+                yAxis: {
+                    axisLabel: 'Nombre de parties jouées',
+                    axisLabelDistance: -10
+                }
+            }
+        };
+
+        $scope.dailygames = [
+            {
+                key: "Cumulative Return",
+                values: [
+                    {
+                        "label" : "Lundi" ,
+                        "value" : 28742
+                    } ,
+                    {
+                        "label" : "Mardi" ,
+                        "value" : 19347
+                    } ,
+                    {
+                        "label" : "Mercredi" ,
+                        "value" : 16342
+                    } ,
+                    {
+                        "label" : "Jeudi" ,
+                        "value" : 28669
+                    } ,
+                    {
+                        "label" : "Vendredi" ,
+                        "value" : 27878
+                    } ,
+                    {
+                        "label" : "Samedi" ,
+                        "value" : 35368
+                    } ,
+                    {
+                        "label" : "Dimanche" ,
+                        "value" : 31465
+                    }
+                ]
+            }
+        ];
 }]);
